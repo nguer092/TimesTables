71a6033e067637f24cb971b0867fab829d07a4f3
Nicolas Guerrero <nick@Nicolass-MacBook-Pro.local>
2017-07-31 13:25:56 -0400

    Initial Commit

---
+++ Times Tables/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Times Tables
+//
+//  Created by Nicolas Guerrero on 7/31/17.
+//  Copyright © 2017 Nicolas Guerrero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
